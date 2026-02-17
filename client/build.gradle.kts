87214beca483f7adcafcfad57a476faa948b0181
ahaodev <52927295@qq.com>
2026-02-17 14:29:25 +0800

    update lib

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -8,15 +8,3 @@ plugins {
     alias(libs.plugins.navigation.safeargs) apply false
     alias(libs.plugins.sentry) apply false
 }
-
-tasks.register<Exec>("aconnectMuMUu") {
-    if (org.gradle.internal.os.OperatingSystem.current().isWindows) {
-        commandLine("adb", "kill-server")
-        doLast {
-            exec { commandLine("adb", "start-server") }
-            exec { commandLine("adb", "connect", "127.0.0.1:7555") }
-        }
-    } else {
-        commandLine("sh", "-c", "adb kill-server && adb start-server && adb connect 127.0.0.1:7555")
-    }
-}
