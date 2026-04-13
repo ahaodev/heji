83e2c55d39777c49afb881e5db019fc567cd6e8f
ahaodev <52927295@qq.com>
2026-04-13 19:59:19 +0800

    java > kotlin

--- app/src/main/java/com/hao/heji/MyAppGlideModule.kt
+++ app/src/main/java/com/hao/heji/MyAppGlideModule.kt
@@ -1,59 +1,51 @@
-package com.hao.heji;
+package com.hao.heji
 
-import android.content.Context;
-import android.net.Uri;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.Registry;
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
-import com.bumptech.glide.load.model.GlideUrl;
-import com.bumptech.glide.module.AppGlideModule;
-import com.hao.heji.network.HttpRetrofit;
-
-import java.io.File;
-import java.io.InputStream;
-
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
+import android.content.Context
+import android.net.Uri
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.module.AppGlideModule
+import com.hao.heji.network.HttpRetrofit
+import java.io.File
+import java.io.InputStream
 
 @GlideModule
-public final class MyAppGlideModule extends AppGlideModule {
+class MyAppGlideModule : AppGlideModule() {
 
-    /**
-     * @param pathName getExternalCacheDir() + "/image.jpg"
-     */
-    public static void loadImageFile(Context context, String pathName, ImageView imgView) {
-        // 加载本地图片
-        File file = new File(pathName);
-        Glide.with(App.Companion.getContext()).load(file).into(imgView);
+    companion object {
+        @JvmStatic
+        fun loadImageFile(context: Context, pathName: String, imgView: ImageView) {
+            val file = File(pathName)
+            Glide.with(context).load(file).into(imgView)
+        }
+
+        @JvmStatic
+        fun loadImageRes(context: Context, resource: Int, imgView: ImageView) {
+            Glide.with(context).load(resource).into(imgView)
+        }
+
+        @JvmStatic
+        fun loadImageBytes(context: Context, image: ByteArray, imgView: ImageView) {
+            Glide.with(context).load(image).into(imgView)
+        }
+
+        @JvmStatic
+        fun loadImageUri(context: Context, imageUri: Uri, imgView: ImageView) {
+            Glide.with(context).load(imageUri).into(imgView)
+        }
+
+        @JvmStatic
+        fun loadImageUrl(context: Context, imageUri: String, imgView: ImageView) {
+            Glide.with(context).load(imageUri).into(imgView)
+        }
     }
 
-    public static void loadImageRes(Context context, int resource, ImageView imgView) {
-        // 加载应用资源
-        Glide.with(context).load(resource).into(imgView);
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        val builder = HttpRetrofit.okHttpClient(15, 15, 15).newBuilder()
+        registry.append(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(builder.build()))
     }
-
-    public static void loadImageBytes(Context context, byte[] image, ImageView imgView) {
-        // 加载二进制流
-        Glide.with(context).load(image).into(imgView);
-    }
-
-    public static void loadImageUri(Context context, Uri imageUri, ImageView imgView) {
-        // 加载Uri对象
-        Glide.with(context).load(imageUri).into(imgView);
-    }
-
-    public static void loadImageUrl(Context context, String imageUri, ImageView imgView) {
-        // 加载Uri对象
-        Glide.with(context).load(imageUri).into(imgView);
-    }
-
-    @Override
-    public void registerComponents(Context context, Glide glide, Registry registry) {
-        final OkHttpClient.Builder builder = HttpRetrofit.INSTANCE.okHttpClient(15, 15, 15).newBuilder();
-        registry.append(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory((Call.Factory) builder.build()));
-    }
-
 }
